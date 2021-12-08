02c94a79931abf8b632b37cc6c7112e5edd03761
OussamaKhouya <oukhouya0b@gmail.com>
2021-12-08 22:31:17 +0100

    prepare to deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:  {
+    apiKey: "AIzaSyCxjmJQGy4_kzIy2zJgETuvRsYfD-A8ftw",
+    authDomain: "angulargestioncontact.firebaseapp.com",
+    projectId: "angulargestioncontact",
+    storageBucket: "angulargestioncontact.appspot.com",
+    messagingSenderId: "1065179152495",
+    appId: "1:1065179152495:web:2575a2b1729f2d7e2fcd9a",
+    measurementId: "G-MERYR0QRE9"
+  }
 };
